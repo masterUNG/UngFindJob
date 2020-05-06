94f655e1bbadedbef1fa0a08c29e1fa87211ae51
masterUNG <phrombutr@gmail.com>
2020-05-06 18:24:55 +0700

    ีupdate

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ungfindjob/utility/my_style.dart';
+import 'package:ungfindjob/utility/normal_dialog.dart';
 
 class Register extends StatefulWidget {
   @override
@@ -8,7 +11,7 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   // Field
-  String type;
+  String type, name, user, password, uid;
 
   @override
   Widget build(BuildContext context) {
@@ -34,10 +37,61 @@ class _RegisterState extends State<Register> {
   }
 
   FloatingActionButton registerButton() => FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          if (name == null ||
+              name.isEmpty ||
+              user == null ||
+              user.isEmpty ||
+              password == null ||
+              password.isEmpty) {
+            normalDialog(context, 'Have Space', 'Please Fill Every Blank');
+          } else if (type == null) {
+            normalDialog(
+                context, 'Non Choose Type', 'Please Tap Company or Student');
+          } else {
+            registerThread();
+          }
+        },
         backgroundColor: MyStyle().primaryColor,
-        child: Icon(Icons.cloud_upload, size: 36.0,),
+        child: Icon(
+          Icons.cloud_upload,
+          size: 36.0,
+        ),
       );
+
+  Future<void> registerThread() async {
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    await firebaseAuth
+        .createUserWithEmailAndPassword(email: user, password: password)
+        .then((value) {
+      FirebaseUser firebaseUser = value.user;
+      uid = firebaseUser.uid;
+      print('Success Register Uid = $uid');
+      insertValueFirestore();
+    }).catchError((value) {
+      String title = value.code;
+      String message = value.message;
+      normalDialog(context, title, message);
+    });
+  }
+
+  Future<void> insertValueFirestore() async {
+    Firestore firestore = Firestore.instance;
+    CollectionReference collectionReference = firestore.collection(type);
+
+    Map<String, dynamic> map = Map();
+    map['Name'] = name;
+    map['Email'] = user;
+    map['Uid'] = uid;
+
+    await collectionReference
+        .document(uid)
+        .setData(map)
+        .then((value) {
+          print('Insert Success');
+        })
+        .catchError((value) {});
+  }
 
   Widget companyRadio() => Row(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -94,6 +148,7 @@ class _RegisterState extends State<Register> {
         Container(
           width: 250.0,
           child: TextField(
+            onChanged: (value) => name = value.trim(),
             decoration: InputDecoration(
                 labelStyle: TextStyle(color: MyStyle().darkColor),
                 labelText: 'Name :',
@@ -113,6 +168,8 @@ class _RegisterState extends State<Register> {
         Container(
           width: 250.0,
           child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            onChanged: (value) => user = value.trim(),
             decoration: InputDecoration(
                 labelStyle: TextStyle(color: MyStyle().darkColor),
                 labelText: 'User :',
@@ -132,6 +189,7 @@ class _RegisterState extends State<Register> {
         Container(
           width: 250.0,
           child: TextField(
+            onChanged: (value) => password = value.trim(),
             decoration: InputDecoration(
                 labelStyle: TextStyle(color: MyStyle().darkColor),
                 labelText: 'Password :',
